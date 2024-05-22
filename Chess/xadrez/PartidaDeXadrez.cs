261d7c1257931ccc80cb2df21ab950f79122d1bc
Victor <victorfariasfer@gmail.com>
2024-05-22 08:07:26 -0300

    jogada especial - promoção - adicionada

--- Chess/xadrez/PartidaDeXadrez.cs
+++ Chess/xadrez/PartidaDeXadrez.cs
@@ -90,6 +90,23 @@ namespace xadrez
                 DesfazerMovimento(origem, destino, pecaCapturada);
                 throw new Exception("Você não pode se colocar em xeque");
             }
+
+            Peca p = Tab.Peca(destino);
+            
+            //jogada espcial a promoção
+
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -109,7 +126,7 @@ namespace xadrez
                 MudaJogador();
             }
 
-            Peca p = Tab.Peca(destino);
+            
 
             // Jogada especial -  en passant
             if(p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
